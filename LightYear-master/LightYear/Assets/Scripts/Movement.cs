e66ecee1eb4d9b85df2eec07d5ef2fd3347ce50f
mericonel <mericonel@hotmail.com>
2016-11-12 15:26:45 -0500

    removed library files

--- LightYear-master/LightYear/Assets/Scripts/Movement.cs
+++ LightYear-master/LightYear/Assets/Scripts/Movement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	private float speed = 3;
+	private float speed = 5;
 
 	// Use this for initialization
 	void Start () {
--- README.md
+++
@@ -1,2 +0,0 @@
-# LightYear
-Final Project for Core Lab and Studio, Fall 2016.
